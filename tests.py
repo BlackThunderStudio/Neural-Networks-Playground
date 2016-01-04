2e0a37fa54b34edb1c6d2fe97f0fba9757a9ae11
OmerKirnap <okirnap@ku.edu.tr>
2016-01-04 02:18:45 +0200

    [WIP] weight update

--- tests.py
+++ tests.py
@@ -7,5 +7,14 @@ if __name__ == '__main__':
     # Create Training data
     train_data = sio.loadmat('ex4data1.mat')
     first_network = NN([400, 25, 10])
+    input_size = len(train_data['X']) ** -1
+    train_parameters = {'train_data':train_data,
+                        'step_size':0.02,
+                        'input_size':input_size,
+                        'lambda':0.012,
+                        'num_of_iterations': 100}
 
-    first_network.back_propagate(train_data=train_data)
+    # first_network._forward_propagate(train_data['X'][1])
+    # a,b =first_network.back_propagate(train_data=train_data)
+    first_network.train(**train_parameters)
+
